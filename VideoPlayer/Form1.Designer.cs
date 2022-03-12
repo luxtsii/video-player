e46c7f8aca920d2af37bb235b5bf5a3b12fdb0eb
Luxtsii <venndatio@gmail.com>
2022-03-12 14:55:59 +0000

    Add project files.

---
+++ VideoPlayer/Form1.Designer.cs
@@ -0,0 +1,157 @@
+﻿namespace VideoPlayer
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.axWindowsMediaPlayer1 = new AxWMPLib.AxWindowsMediaPlayer();
+            this.loadVideo = new System.Windows.Forms.Button();
+            this.videoDialog = new System.Windows.Forms.OpenFileDialog();
+            this.backFrame = new System.Windows.Forms.Button();
+            this.forwardFrame = new System.Windows.Forms.Button();
+            this.startBtn = new System.Windows.Forms.CheckBox();
+            this.endBtn = new System.Windows.Forms.CheckBox();
+            this.loopBtn = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.axWindowsMediaPlayer1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // axWindowsMediaPlayer1
+            // 
+            this.axWindowsMediaPlayer1.Dock = System.Windows.Forms.DockStyle.Top;
+            this.axWindowsMediaPlayer1.Enabled = true;
+            this.axWindowsMediaPlayer1.Location = new System.Drawing.Point(0, 0);
+            this.axWindowsMediaPlayer1.Name = "axWindowsMediaPlayer1";
+            this.axWindowsMediaPlayer1.OcxState = ((System.Windows.Forms.AxHost.State)(resources.GetObject("axWindowsMediaPlayer1.OcxState")));
+            this.axWindowsMediaPlayer1.Size = new System.Drawing.Size(1314, 759);
+            this.axWindowsMediaPlayer1.TabIndex = 0;
+            this.axWindowsMediaPlayer1.Enter += new System.EventHandler(this.axWindowsMediaPlayer1_Enter);
+            // 
+            // loadVideo
+            // 
+            this.loadVideo.Location = new System.Drawing.Point(10, 765);
+            this.loadVideo.Name = "loadVideo";
+            this.loadVideo.Size = new System.Drawing.Size(75, 23);
+            this.loadVideo.TabIndex = 1;
+            this.loadVideo.Text = "Load Video";
+            this.loadVideo.UseVisualStyleBackColor = true;
+            this.loadVideo.Click += new System.EventHandler(this.loadVideo_Click);
+            // 
+            // videoDialog
+            // 
+            this.videoDialog.FileName = "videoDialog";
+            // 
+            // backFrame
+            // 
+            this.backFrame.Location = new System.Drawing.Point(92, 765);
+            this.backFrame.Name = "backFrame";
+            this.backFrame.Size = new System.Drawing.Size(28, 23);
+            this.backFrame.TabIndex = 2;
+            this.backFrame.Text = "<";
+            this.backFrame.UseVisualStyleBackColor = true;
+            this.backFrame.Click += new System.EventHandler(this.backFrame_Click);
+            // 
+            // forwardFrame
+            // 
+            this.forwardFrame.Location = new System.Drawing.Point(126, 765);
+            this.forwardFrame.Name = "forwardFrame";
+            this.forwardFrame.Size = new System.Drawing.Size(28, 23);
+            this.forwardFrame.TabIndex = 3;
+            this.forwardFrame.Text = ">";
+            this.forwardFrame.UseVisualStyleBackColor = true;
+            this.forwardFrame.Click += new System.EventHandler(this.forwardFrame_Click);
+            // 
+            // startBtn
+            // 
+            this.startBtn.AutoSize = true;
+            this.startBtn.Location = new System.Drawing.Point(160, 769);
+            this.startBtn.Name = "startBtn";
+            this.startBtn.Size = new System.Drawing.Size(48, 17);
+            this.startBtn.TabIndex = 4;
+            this.startBtn.Text = "Start";
+            this.startBtn.UseVisualStyleBackColor = true;
+            this.startBtn.CheckedChanged += new System.EventHandler(this.startBtn_CheckedChanged_1);
+            // 
+            // endBtn
+            // 
+            this.endBtn.AutoSize = true;
+            this.endBtn.Location = new System.Drawing.Point(214, 769);
+            this.endBtn.Name = "endBtn";
+            this.endBtn.Size = new System.Drawing.Size(45, 17);
+            this.endBtn.TabIndex = 5;
+            this.endBtn.Text = "End";
+            this.endBtn.UseVisualStyleBackColor = true;
+            this.endBtn.CheckedChanged += new System.EventHandler(this.endBtn_CheckedChanged_1);
+            // 
+            // loopBtn
+            // 
+            this.loopBtn.Location = new System.Drawing.Point(265, 765);
+            this.loopBtn.Name = "loopBtn";
+            this.loopBtn.Size = new System.Drawing.Size(75, 23);
+            this.loopBtn.TabIndex = 6;
+            this.loopBtn.Text = "Loop";
+            this.loopBtn.UseVisualStyleBackColor = true;
+            this.loopBtn.Click += new System.EventHandler(this.loopBtn_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1314, 800);
+            this.Controls.Add(this.loopBtn);
+            this.Controls.Add(this.endBtn);
+            this.Controls.Add(this.startBtn);
+            this.Controls.Add(this.forwardFrame);
+            this.Controls.Add(this.backFrame);
+            this.Controls.Add(this.loadVideo);
+            this.Controls.Add(this.axWindowsMediaPlayer1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.axWindowsMediaPlayer1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        private void AxWindowsMediaPlayer1_PlayStateChange(object sender, AxWMPLib._WMPOCXEvents_PlayStateChangeEvent e)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        #endregion
+
+        private AxWMPLib.AxWindowsMediaPlayer axWindowsMediaPlayer1;
+        private System.Windows.Forms.Button loadVideo;
+        private System.Windows.Forms.OpenFileDialog videoDialog;
+        private System.Windows.Forms.Button backFrame;
+        private System.Windows.Forms.Button forwardFrame;
+        private System.Windows.Forms.CheckBox startBtn;
+        private System.Windows.Forms.CheckBox endBtn;
+        private System.Windows.Forms.Button loopBtn;
+    }
+}
+
---
+++ VideoPlayer/Form1.cs
@@ -0,0 +1,123 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Timers;
+using System.Windows.Forms;
+using WMPLib;
+
+namespace VideoPlayer
+{
+    public partial class Form1 : Form
+    {
+        string vid;
+        double start = 69, end = 69;
+
+        public Form1()
+        {
+            InitializeComponent();
+
+            System.Timers.Timer t = new System.Timers.Timer();
+            t.Interval = 5; // In milliseconds
+            t.AutoReset = true; // Stops it from repeating
+            t.Elapsed += new ElapsedEventHandler(loopTime);
+            t.Start();
+        }
+
+        private void loadVideo_Click(object sender, EventArgs e)
+        {
+            videoDialog.ShowDialog();
+            vid = videoDialog.FileName;
+
+            axWindowsMediaPlayer1.URL = vid;
+            axWindowsMediaPlayer1.Refresh();
+            axWindowsMediaPlayer1.settings.rate = 0.01f;
+        }
+
+        private void axWindowsMediaPlayer1_Enter(object sender, EventArgs e)
+        {
+
+
+        }
+
+        private void THISISEPIC(object sender, AxWMPLib._WMPOCXEvents_PlayStateChangeEvent e)
+        {
+
+        }
+
+        private void loopTime(object sender, ElapsedEventArgs e)
+        {
+            try
+            {
+                if (start != 69 && end != 69)
+                {
+                    Console.WriteLine("Current Pos: " + axWindowsMediaPlayer1.Ctlcontrols.currentPosition);
+                    if (axWindowsMediaPlayer1.Ctlcontrols.currentPosition > start)
+                    {
+                        Console.WriteLine("Returning");
+                        axWindowsMediaPlayer1.Ctlcontrols.currentPosition = end;
+                        
+                    }
+                }
+
+            } catch (Exception)
+            {
+
+            }   
+
+        }
+
+
+        private void forwardFrame_Click(object sender, EventArgs e)
+        {
+            ((WMPLib.IWMPControls2)axWindowsMediaPlayer1.Ctlcontrols).step(1);
+            //axWindowsMediaPlayer1.Refresh();
+        }
+
+        private void backFrame_Click(object sender, EventArgs e)
+        {
+            IWMPControls2 Ctlcontrols2 = (IWMPControls2)axWindowsMediaPlayer1.Ctlcontrols;
+
+            double frameRate = axWindowsMediaPlayer1.network.encodedFrameRate;
+            double step = 1.0 / frameRate;
+
+            axWindowsMediaPlayer1.Ctlcontrols.pause();
+            axWindowsMediaPlayer1.Ctlcontrols.currentPosition -= step; //Go backwards
+            axWindowsMediaPlayer1.Ctlcontrols.pause();
+
+        }
+
+        private void endBtn_CheckedChanged(object sender, EventArgs e)
+        {
+            end = axWindowsMediaPlayer1.Ctlcontrols.currentPosition;
+            Console.WriteLine(end);
+        }
+
+        private void startBtn_CheckedChanged_1(object sender, EventArgs e)
+        {
+            end = axWindowsMediaPlayer1.Ctlcontrols.currentPosition;
+            Console.WriteLine(end);
+        }
+
+        private void endBtn_CheckedChanged_1(object sender, EventArgs e)
+        {
+            start = axWindowsMediaPlayer1.Ctlcontrols.currentPosition;
+            Console.WriteLine(start);
+        }
+
+        private void loopBtn_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void startBtn_CheckedChanged(object sender, EventArgs e)
+        {
+            start = axWindowsMediaPlayer1.Ctlcontrols.currentPosition;
+            Console.WriteLine(start);
+        }
+    }
+}
